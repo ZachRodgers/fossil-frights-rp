c54a6db89493a1d562bd7e4d9a84c9d73843a70b
Evtema3 <evtema3@gmail.com>
2026-06-30 13:45:24 -0400

    Remove shader selector and transparency post effect, as they will not be supported in 26.3

--- assets/shader_selector/shaders/include/data_reader.glsl
+++
@@ -1,8 +0,0 @@
-
-float readChannel(int channel) {
-    return decodeColor(texelFetch(DataSampler, ivec2(4, channel), 0));
-}
-
-float readTime(int channel) {
-    return decodeColor(texelFetch(DataSampler, ivec2(1, channel), 0));
-}
